482d5466474746540206c9f34c3c43cf99c7268b
Kumar <m.kumarmohit19@gmail.com>
2022-03-15 22:36:16 +0530

    length of longest substring problem solved

---
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -0,0 +1,26 @@
+// Given a string, return the length of the longest substring without
+// repeating characters.
+// --- Example
+// lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
+// lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
+
+function lengthOfLongestSubstring(s) {
+   let windowsCharsMap = {};
+   let windowStart = 0;
+   let maxLength = 0;
+
+   for ( let i = 0; i < s.length; i++ ) {
+      const endChar = s[i];
+
+      if(windowsCharsMap[endChar] >= windowStart) {
+         windowStart = windowsCharsMap[endChar] + 1;
+      }
+      windowsCharsMap[endChar] = i;
+      
+      maxLength = Math.max(maxLength, i - windowStart + 1);
+   }
+
+   return maxLength;
+}
+
+module.exports = lengthOfLongestSubstring;
